8884d647cfbc4591e90068dd831d259c66bfe9be
조보화 <jbh@x2soft.co.kr>
2022-04-11 19:40:40 +0900

    build file add

---
+++ docs/_nuxt/0b45c20.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{281:function(e,n,t){"use strict";t.r(n);var l=t(3).default.extend({name:"IndexPage"}),c=t(63),component=Object(c.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("\n  test\n")])}),[],!1,null,null,null);n.default=component.exports}}]);
