5fece6a65f08b89b1058b641d2a704ec456fc6b3
조보화 <jbh@x2soft.co.kr>
2022-04-30 00:37:35 +0900

    영화 데이터 추가

--- pages/detail/music.vue
+++ pages/detail/music.vue
@@ -11,6 +11,7 @@
           <g-button v-for="(option, index) in mainInfo.subject"
                     :is-gray="true"
                     :key="`subject_${index}`"
+                    :class="{ 'is-active': subject.value === option.value }"
                     @click="setSubject(option.value, option.label)">{{ option.label }}</g-button>
         </div>
       </div>
@@ -20,6 +21,7 @@
           <g-button v-for="(option, index) in questionNumberList"
                     :is-gray="true"
                     :key="`question_${index}`"
+                    :class="{ 'is-active': questionCount === option.value }"
                     @click="setQuestionCount(option.value)">{{ option.label }}</g-button>
         </div>
       </div>
